85ad843946db46f40fbf7365d4684ed08e2aad13
revenantonthemission <corpseonthemission@icloud.com>
2026-02-12 11:07:26 +0900

    refactor: AWS S3 연결

--- main.py
+++ main.py
@@ -11,44 +11,44 @@ app.mount("/js", StaticFiles(directory="js"), name="js")
 
 @app.get("/")
 async def read_root():
-    return FileResponse("html/user_login.html")
+    return FileResponse("user_login.html")
 
 
 @app.get("/main")
 async def main_page():
-    return FileResponse("html/post_list.html")
+    return FileResponse("post_list.html")
 
 
 @app.get("/login")
 async def login():
-    return FileResponse("html/user_login.html")
+    return FileResponse("user_login.html")
 
 
 @app.get("/signup")
 async def signup():
-    return FileResponse("html/user_signup.html")
+    return FileResponse("user_signup.html")
 
 
 @app.get("/password")
 async def password():
-    return FileResponse("html/user_password.html")
+    return FileResponse("user_password.html")
 
 
 @app.get("/detail")
 async def detail():
-    return FileResponse("html/post_detail.html")
+    return FileResponse("post_detail.html")
 
 
 @app.get("/write")
 async def write():
-    return FileResponse("html/post_write.html")
+    return FileResponse("post_write.html")
 
 
 @app.get("/edit")
 async def edit():
-    return FileResponse("html/post_edit.html")
+    return FileResponse("post_edit.html")
 
 
 @app.get("/edit-profile")
 async def edit_profile():
-    return FileResponse("html/user_edit.html")
+    return FileResponse("user_edit.html")
